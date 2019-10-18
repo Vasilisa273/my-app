4e9674447c81021652f15c4e55666593ffd88925
Jenek <lavochka66@gmail.com>
2019-10-18 17:19:14 +0300

    OnePage

---
+++ src/OnePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class OnePage extends Component {
+
+    render() {
+        return (
+            <button
+                className = "button">
+                Sign in
+                Sign up
+            </button>
+        )
+    }
+}
+export default OnePage;
